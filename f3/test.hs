2594066f744b34527adaee602087bee0eb9f4f1f
Lol4t0 <sidorov.ij@gmail.com>
2015-01-27 23:47:23 +0300

    fix haskell. Speed up in ~30 times

--- f3/test.hs
+++ f3/test.hs
@@ -1,6 +1,12 @@
-cmpsum i j k =
-    if i + j == k then 1 else 0
+cmpsum::Int->Int->Int->Int
+cmpsum i j k
+    | i + j == k = 1
+    | otherwise = 0
 
-main = print (sum([cmpsum i j k |
-    i <- [1..1000], j <- [1..1000], k <- [1..1000]]))
+next i j 1001 acc = next i (j+1) 1 acc
+next i 1001 k acc = next (i+1) 1 k acc
+next 1001 _ _ acc = acc
+next i j k acc = next i j (k+1) (cmpsum i j k + acc)
+
+main = print $! next 1 1 1 0
 
